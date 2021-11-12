9b607fa068a6ee84c045d937f5b02c523e69ffbc
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-11-12 16:50:46 +0000

    refactor

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -5,7 +5,7 @@ import './SelectCharacter.css';
 const SelectCharacter = ({ setCharacterNFT }) => {
   return (
     <div className="select-character-container">
-      <h2>Mint Your Hero. Choose wisely.</h2>
+      <h2>Mint Your Athlete. Choose wisely.</h2>
     </div>
   );
 };
